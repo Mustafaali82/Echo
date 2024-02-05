7a9a3f7005ee06c07842114133c7f51feb330d3e
Mustafa Ali <mustafaali8254@gmail.com>
2024-02-05 16:28:03 +0500

    Header Updated

---
+++ index.js
@@ -0,0 +1,8 @@
+function munuFunction() {
+  var x = document.getElementById("navigationMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
